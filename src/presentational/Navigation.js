ee94de7789c552573902d2f877b020fe9e049bd5
Ilona Pastucha <ilonka.p4@gmail.com>
2018-11-13 20:38:59 +0100

    Add app files

---
+++ src/presentational/Navigation.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Navigation = (props) => {
+    return (
+        <div className="nav">
+            <button onClick={() => props.newGameHandler()}>New Game</button>
+            <button onClick={() => props.restartNewGame()}>Restart</button>
+            <button onClick={() => props.solveSudoku()}>Solve</button>
+            <button onClick={() => props.checkSudoku()}>Check</button>
+            {
+                props.error ? <div><p className="error-msg">{props.error}</p></div> : null
+            }
+            {
+                props.newGameClicked ? 
+                <div>
+                    <h1>Select level:</h1>
+                    <button onClick={(e) => props.startNewGame(e.target.textContent)}>easy</button>
+                    <button onClick={(e) => props.startNewGame(e.target.textContent)}>medium</button>
+                    <button onClick={(e) => props.startNewGame(e.target.textContent)}>hard</button>
+                    <button onClick={(e) => props.startNewGame(e.target.textContent)}>very-hard</button>
+                    <button onClick={(e) => props.startNewGame(e.target.textContent)}>insane</button>
+                    <button onClick={(e) => props.startNewGame(e.target.textContent)}>inhuman</button>
+                </div> : null
+            }
+        </div>
+    )
+}
+
+export default Navigation;
---
+++ src/presentational/Title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Title = () => {
+    return (
+        <div className="title">
+            <h1><span className="game-title">Sudoku Game</span></h1>
+        </div>
+    )
+}
+
+export default Title;
